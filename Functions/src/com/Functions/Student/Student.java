11f7ede91d6d539f985dfeaaae20aa7e7f64501c
swathitekumatla <tekumatlaswathi7@gmail.com>
2023-10-28 19:01:53 +0530

    Functions

---
+++ Functions/src/com/Functions/Student/Student.java
@@ -0,0 +1,14 @@
+package com.Functions.Student;
+
+public class Student {
+
+	public static void main(String[] args)
+	{
+		StudentDetails obj=new StudentDetails();
+		obj.setName("swathi");
+		obj.setrollNo(1);
+		obj.setRank(1);
+		System.out.println(obj.getName()+"\n"+obj.getrollNo()+"\n"+obj.getRank());
+	}
+
+}
---
+++ Functions/src/com/Functions/Student/StudentDetails.java
@@ -0,0 +1,33 @@
+package com.Functions.Student;
+
+public class StudentDetails 
+{
+	private String name;
+	private int rollNo;
+	private int rank;
+  public void setName(String name)
+  			{
+	  			this.name=name;
+  			}
+			public String getName()
+			{
+				return name;
+			}
+			public void setrollNo(int rollNo)
+			{
+				this.rollNo=rollNo;
+			}
+			public int getrollNo()
+			{
+				return rollNo;
+			}
+			public void setRank(int rank)
+			{
+				this.rank=rank;
+			}
+			public int getRank()
+			{
+				return rank;
+			}
+			
+			}
