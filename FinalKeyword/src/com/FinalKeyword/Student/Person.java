2a81263ef5dada58e1730f4809aa1c44ea802d81
swathitekumatla <tekumatlaswathi7@gmail.com>
2023-10-28 18:15:32 +0530

    Final Keyword

---
+++ FinalKeyword/src/com/FinalKeyword/Student/Person.java
@@ -0,0 +1,12 @@
+package com.FinalKeyword.Student;
+
+final public class Person  // can not inherited
+{
+final int x=10;   // can not change the variable
+final void getNumber()  // can not override the method
+{
+	String name="swathi";
+	System.out.println("My name is :"+name);
+	System.out.println(x);
+}
+}
---
+++ FinalKeyword/src/com/FinalKeyword/Student/Student.java
@@ -0,0 +1,13 @@
+package com.FinalKeyword.Student;
+
+public class Student
+{
+
+	public static void main(String[] args) 
+	{
+		Person ob=new Person();
+		ob.getNumber();
+
+	}
+
+}
